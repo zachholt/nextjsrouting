9f636fbc7c9a0ccebd711dc469101f87438add34
Zach <zacjac96@icloud.com>
2024-08-13 20:32:05 -0500

    add random number to page

--- src/app/internal3/page.tsx
+++ src/app/internal3/page.tsx
@@ -3,5 +3,5 @@ import Link from "next/link";
 export default function InternalThree() {
     const randomNumber = Math.floor(Math.random() * 1000);
 
-    return <Link href={`/dynamic/${randomNumber}`}>INTERNAL PAGE THREE! CLICK ME TO GO DYNAMIC!</Link>;
+    return <Link href={`/dynamic/${randomNumber}`}>INTERNAL PAGE THREE! CLICK ME TO GO DYNAMIC {randomNumber}!</Link>;
 }
